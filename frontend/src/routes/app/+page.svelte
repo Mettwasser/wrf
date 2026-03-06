d28d5a4237d6512400e84591aa18035218078bb7
Mettwasser <kaicrafter2000@gmail.com>
2026-03-06 23:10:06 +0100

    feat: add simple filter

--- frontend/src/routes/app/+page.svelte
+++ frontend/src/routes/app/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-    import { identity } from '$lib';
+    import { identity, toaster } from '$lib';
     import LobbyCreateButton from '$lib/components/LobbyCreateButton.svelte';
     import LobbyItem from '$lib/components/LobbyItem.svelte';
+    import LobbyQuickMatchButton from '$lib/components/LobbyQuickMatchButton.svelte';
     import { tables } from '$lib/module_bindings/index.js';
     import type { Lobby, User } from '$lib/module_bindings/types.js';
     import type { LobbyAndUser } from '$lib/types/lobby_and_user.js';
@@ -23,11 +24,21 @@
         lobby: Lobby;
     }
 
+    let relicFilter = $state('');
+
     const allLobbiesWithUsers: OptionalLobbyAndUser[] = $derived(
-        $lobbies.map((lobby) => ({
-            lobby,
-            user: $users.find((u) => u.id.equals(lobby.host)),
-        }))
+        $lobbies
+            .filter((lobby) => {
+                console.log('Lobby', lobby.activity.toLowerCase());
+                return (
+                    relicFilter === '' ||
+                    lobby.activity.toLowerCase().includes(relicFilter.toLowerCase())
+                );
+            })
+            .map((lobby) => ({
+                lobby,
+                user: $users.find((u) => u.id.equals(lobby.host)),
+            }))
     );
 
     const myLobby: OptionalLobbyAndUser | undefined | null = $derived(
@@ -48,10 +59,19 @@
 </script>
 
 <div class="mt-8 flex flex-1 flex-col items-center gap-16">
-    <div class="flex w-2/3 flex-col">
-        <div class="flex gap-4 max-sm:flex-col">
-            <input type="text" class="input" placeholder="Search for a relic" />
+    <div class="flex w-2/3">
+        <div class="flex w-full gap-4 max-sm:flex-col">
+            <input
+                type="text"
+                class="input w-full"
+                placeholder="Search for a relic"
+                bind:value={relicFilter}
+            />
             <LobbyCreateButton {relics} hasLobbyOpen={myLobby !== undefined && myLobby !== null} />
+            <LobbyQuickMatchButton
+                {relics}
+                hasLobbyOpen={myLobby !== undefined && myLobby !== null}
+            />
         </div>
     </div>
     <ul class="flex w-full flex-col flex-wrap items-center justify-center gap-8 lg:flex-row">
