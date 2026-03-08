aced4f463ec36e7a2b14024adf31301fd2d724fb
Mettwasser <kaicrafter2000@gmail.com>
2026-03-08 15:59:16 +0100

    fix: login redirect issues

--- frontend/src/routes/(auth)/login/[...catchall]/+page.svelte
+++ frontend/src/routes/(auth)/login/[...catchall]/+page.svelte
@@ -1,17 +1,14 @@
 <script lang="ts">
     import { dark } from '@clerk/themes';
-    import { SignedIn, SignedOut, SignIn, SignOutButton } from 'svelte-clerk';
+    import { Show, SignIn, SignOutButton } from 'svelte-clerk';
 </script>
 
-<SignedOut>
-    <SignIn
-        appearance={{ captcha: { theme: 'dark' }, theme: dark }}
-        forceRedirectUrl="/app/next_steps"
-    />
-</SignedOut>
-<SignedIn>
+<Show when="signed-out">
+    <SignIn forceRedirectUrl="/app/next_steps" appearance={{ theme: dark }} />
+</Show>
+<Show when="signed-in">
     <div class="flex flex-col items-center gap-4">
         <h1 class="h2">You are already logged in!</h1>
         <SignOutButton class="btn preset-filled-error-500 w-2/3" />
     </div>
-</SignedIn>
+</Show>
